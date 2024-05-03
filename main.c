f67bfa651fcea0cae00ed1fec1c8014667c07d59
USG-SW1 <mchen@zyxel.com.tw>
2024-05-03 10:02:50 +0000

    bug fix

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include "stdio.h"
+#include <string.h> // Add missing import
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-  /// @brief 
-  printf("This is test\n");
+  char name[1024];;
+  char* token;
+
+  printf("Enter student's name: ");
+  scanf("%[^\n]", name); // Read the entire line including spaces
+
+  // Check if name contains at least two tokens
+  token = strtok(name, " ");
+  if (token == NULL || strtok(NULL, " ") == NULL) {
+    printf("Error: Name should contain at least first name and last name.\n");
+    return 1; // Return error
+  }
+
+  int id;
+
+  printf("Enter student's ID: ");;
+  scanf("%d", &id);
+
+  printf("Student's name: %s\n", name);
+  printf("Student's ID: %d\n", id);
+
   return 0;
 }
